478076cf2dc7a70f5f796977de09a68031167429
devjava69 <k.trabelsi9@laposte.net>
2024-01-07 16:04:14 +0100

    - add ProductRepository and his interface - update Context structure by adding his interface

---
+++ Catalog.API/Repositories/Interface/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories.Interface
+{
+    public interface IProductRepository
+    {
+        Task CreateProductAsync(Product product);
+        Task<IEnumerable<Product>> GetProductsAsync ();
+        Task<Product> GetProductByIdAsync(string id);
+        Task<IEnumerable<Product>> GetProductByNameAsync(string name);
+        Task<IEnumerable<Product>> GetProductByCategoryAsync(string categoryName);
+        Task<bool> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(string id);
+    }
+}
