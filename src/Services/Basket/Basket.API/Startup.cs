cbd167484911cb1935955fb56f8c55a168bf4fb6
devjava69 <k.trabelsi9@laposte.net>
2024-01-13 23:13:16 +0100

    - update stratup by modifyin the redis connectionstring attribute - add service injection in startup file

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,4 +1,6 @@
 using Basket.API.Repositories;
+using Basket.API.Services;
+using Basket.API.Services.Interface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -28,10 +30,11 @@ namespace Basket.API
         {
             services.AddStackExchangeRedisCache(options =>
             {
-                options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectString");
+                options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
 
             services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddScoped<IBasketService, BasketService>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
