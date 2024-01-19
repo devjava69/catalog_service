f717f1b09f2f9e12a0bb4df1d4ac0bef7c75c85d
devjava69 <k.trabelsi9@laposte.net>
2024-01-19 18:01:59 +0100

    create discount controller - Add Discount controller

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,50 @@
+﻿using Discount.API.Entities;
+using Discount.API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+
+        public DiscountController(IDiscountService discountService)
+        {
+            _discountService = discountService ?? throw new ArgumentNullException(nameof(discountService));
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _discountService.GetDiscount(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _discountService.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            return Ok(await _discountService.UpdateDiscount(coupon));
+        }
+
+        [HttpDelete("{productName}", Name = "DeleteDiscont")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            return Ok(await _discountService.DeleteDiscount(productName));
+        }
+    }
+}
