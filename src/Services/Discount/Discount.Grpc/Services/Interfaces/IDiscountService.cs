85722185bfff18d3d17ec1aee0d93e4e70a81166
devjava69 <k.trabelsi9@laposte.net>
2024-01-25 23:39:53 +0100

    add new project discount grpc add command execute non query after deop table if exists Coupon

---
+++ src/Services/Discount/Discount.Grpc/Services/Interfaces/IDiscountService.cs
@@ -0,0 +1,16 @@
+﻿using Discount.Grpc.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services.Interfaces
+{
+    public interface IDiscountService
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+
+        Task<bool> UpdateDiscount(Coupon coupon);
+
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
